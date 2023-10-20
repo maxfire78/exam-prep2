4f48a4edbd3c003f47fe41400ce9eaff6ed05150
Krastyo Todorov <maxfire@abv.bg>
2023-10-20 19:50:17 +0300

    App

---
+++ Terraform/varibles.tf
@@ -0,0 +1,59 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group Name"
+
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "Resource Group Location"
+
+}
+
+variable "app_service_plan_name" {
+  type        = string
+  description = "Service Plan Name"
+
+}
+
+variable "app_service_name" {
+  type        = string
+  description = "Service Name"
+
+}
+
+variable "sql_server_name" {
+  type        = string
+  description = "sql_server_name "
+
+}
+
+variable "sql_database_name" {
+  type        = string
+  description = "SQL Database Name"
+
+}
+
+variable "sql_administrator_login_username" {
+  type        = string
+  description = "SQL Admin Username"
+
+}
+
+variable "sql_administrator_password" {
+  type        = string
+  description = "SQL Admin Password"
+
+}
+
+variable "firewall_rule_name" {
+  type        = string
+  description = "Firewall Rule"
+
+}
+
+variable "repo_URL" {
+  type        = string
+  description = "Firewall Rule"
+
+}
